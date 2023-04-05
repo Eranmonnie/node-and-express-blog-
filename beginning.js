c826316e382b7fa4070e2d514df7d0c7b7412237
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-05 18:06:32 +0100

    global object like window

--- beginning.js
+++ beginning.js
@@ -2,4 +2,13 @@
 
 global.setTimeout(() => {
     console.log("hii")
+    clearInterval(int)
 }, 3000);
+
+
+
+const int = global.setInterval(() => {
+    console.log("work")
+}, 1000);
+ // setinterval:runs what is inside each time its set 
+// clerinterval: stops program after time 
