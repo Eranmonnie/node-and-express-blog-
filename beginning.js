d05689b204584799a6b325babdbb3754fbd1b277
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-06 15:37:43 +0100

    filesystem in js

--- beginning.js
+++ beginning.js
@@ -1,6 +1,51 @@
-const {name, obj} = require('./module')
-console.log(`${name}, ${obj}`)
+// files
+const fs = require('fs')
 
-const mod = require('./module')
-console.log(mod)
-// can import them all or in pieces
+// raad
+
+fs.readFile("./docs/test.txt", (err, data)=>{
+
+    if (err){
+        console.log(err)
+    }
+    console.log(data.toString())
+})
+
+//write
+
+fs.writeFile("./docs/test2.txt" , "hello world" , ()=>{
+    console.log("new file")
+})
+
+//make directory 
+if (!(fs.existsSync('./directory'))){
+fs.mkdir('./directory', (err)=>{
+    if (err){
+        console.log(err)
+    }
+    console.log("directory created")
+})
+}
+else{
+    fs.rmdir("./directory", (err)=>{
+        if (err){
+            console.log(err)
+        }
+        console.log("directory deleted")
+    }) 
+}
+
+
+//delete directory
+if (fs.existsSync('./deleteme.txt')){
+    fs.unlink('./deleteme.txt', (err)=>{
+        if (err){
+            console.log(err)
+        }
+        console.log('file deleted')
+    })
+}
+else{
+    console.log('file has already been deleted or does not exist')
+}
+//if testing mske deleteme.txt
