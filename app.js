5b3cd0e33a5f5ae886d4086d98465828de220f84
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-07 17:32:36 +0100

    view engine using ejs

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
 
+//useing ejs 
+app.set('view engine', 'ejs')
+
 // listen for requesta at port 3000
 app.listen(3000)
 
 //sending html responses to route
 app.get('/', (req, res)=>{
-    res.sendFile('./views/home.html', {root :__dirname})
+    res.render('home')
 })
 
 app.get('/about', (req, res)=>{
-    res.sendFile('./views/about.html', {root :__dirname})
+    res.render('about')
+})
+
+//redirecting 
+app.get('/about-us', (req, res)=>{
+    res.redirect('/about')
+})
+
+
+//404
+app.use((req, res)=>{
+    res.status(404).render('404')
 })
