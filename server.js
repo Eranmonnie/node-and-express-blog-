fe5979c07fd3ccfe3641d781dd577f49bb5d5f65
Eranmonnie <ajalaferanmi123@gmail.com>
2023-04-07 11:44:00 +0100

    redirecting with raw node

--- server.js
+++ server.js
@@ -10,14 +10,23 @@ const state = http.createServer((req, res)=>{
 
     if (req.url === '/'){
         path += 'home.html'
+        res.statusCode = 200
        }
        
     else if(req.url === '/about'){
     path += 'about.html'
+    res.statusCode = 200
     }
-    
+
+    else if(req.url === '/about-me'){
+        res.statusCode = 301
+        res.setHeader('Location', '/about')
+        res.end()
+        }
+
     else{
         path += '404.html'
+        res.statusCode = 404
         }
    
 
